7cbd79ca2b73fe5b2255ab7fac660eafc15d8316
Levartar <jakob.guenster@gmx.net>
2025-01-12 17:44:40 +0100

    additional children routing fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,37 +6,37 @@ export const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full'},
   {
     path: 'main', component: MainComponent,
-    //children: [
-    //  {
-    //    path: '',
-    //    redirectTo: 'dashboard',
-    //    pathMatch: 'full'
-    //  },
-    //  {
-    //    path: 'dashboard',
-    //    component: DashboardComponent
-    //  },
-    //  {
-    //    path: 'decks',
-    //    redirectTo: 'dashboard'
-    //    //component: DecksComponent,
-    //  },
-    //  {
-    //    path: 'winrate',
-    //    redirectTo: 'dashboard'
-    //    //component: WinrateComponent,
-    //  },
-    //  {
-    //    path: 'meta',
-    //    redirectTo: 'dashboard'
-    //    //component: MetaComponent,
-    //  },
-    //  {
-    //    path: 'warbands',
-    //    redirectTo: 'dashboard'
-    //    //component: WarbandsComponent,
-    //  },
-    //]
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'decks',
+        redirectTo: 'dashboard'
+        //component: DecksComponent,
+      },
+      {
+        path: 'winrate',
+        redirectTo: 'dashboard'
+        //component: WinrateComponent,
+      },
+      {
+        path: 'meta',
+        redirectTo: 'dashboard'
+        //component: MetaComponent,
+      },
+      {
+        path: 'warbands',
+        redirectTo: 'dashboard'
+        //component: WarbandsComponent,
+      },
+    ]
   },
   { path: '**', redirectTo: 'main'}
 ];
